84ccfa04147eb42225550f35072637ed63ce89e5
Pavel Kovalenko <paul-kv@yandex.ru>
2014-08-25 23:20:48 +0400

    Remove OpenAutomate headers and libraries from repository, update paths.

--- cs/engine/xrEngine/xrSASH.h
+++ cs/engine/xrEngine/xrSASH.h
@@ -2,7 +2,7 @@
 #define	xrSASH_included
 #pragma once
 
-#include "OpenAutomate/OpenAutomate.h"
+#include <OpenAutomate/inc/OpenAutomate.h>
 
 //struct oaOptionDependencyStruct;
 //typedef struct oaOptionDependencyStruct oaOptionDependency;
