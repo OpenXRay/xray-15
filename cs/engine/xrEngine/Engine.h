87ca47e3e947aa816450ea31dab1fa90e3f62fe8
Pavel Kovalenko <paul-kv@yandex.ru>
2014-08-26 22:46:01 +0400

    Fix missing forward declarations in xrGame.

--- cs/engine/xrEngine/Engine.h
+++ cs/engine/xrEngine/Engine.h
@@ -10,6 +10,21 @@
 #include "eventAPI.h"
 #include "xrSheduler.h"
 
+struct vertRender;
+struct vertBoned1W;
+struct vertBoned2W;
+struct vertBoned3W;
+struct vertBoned4W;
+class CBoneInstance;
+struct CKey;
+struct CKeyQR;
+struct CKeyQT;
+#ifdef _EDITOR
+#define MATRIX Fmatrix
+template<class T> struct _matrix;
+#define MATRIX _matrix<float>
+#endif
+
 class ENGINE_API CEngine
 {
 public:
