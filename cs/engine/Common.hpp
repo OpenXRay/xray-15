0007e15999636fbf334bd789f2a8a6ff03eb2fb2
Pavel Kovalenko <paul-kv@yandex.ru>
2014-08-28 22:57:42 +0400

    Add common use header with typedefs.

---
+++ cs/engine/Common.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+using uint = unsigned int;
+using uint32 = unsigned int;
+using int32 = signed int;
+
+using ushort = unsigned short;
+using uint16 = unsigned short;
+using int16 = signed short;
+
+using uint64 = unsigned long long;
+using int64 = signed long long;
+
+using sbyte = signed char;
+using int8 = signed char;
+using byte = unsigned char;
+using uint8 = unsigned char;
