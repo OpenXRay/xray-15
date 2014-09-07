545f1741980765a28b10afd2e95ee074ccd30e55
Pavel Kovalenko <paul-kv@yandex.ru>
2014-09-07 22:43:21 +0400

    Add BUG.

--- cs/engine/xrSound/SoundRender_Source.cpp
+++ cs/engine/xrSound/SoundRender_Source.cpp
@@ -62,7 +62,7 @@ void CSoundRender_Source::i_decompress_fr(OggVorbis_File* ovf, char* _dest, u32 
 		if (ret == 0) break;
 		else if (ret < 0) 		// Error in bitstream
 		{
-		//
+		    // BUG: if ov_read returns negative value, this loop wont be leaved
 		}
 		else
 		{
