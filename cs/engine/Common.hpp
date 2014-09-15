84388b8f35cabb5fb07e8d0ffad90e08ae584381
Pavel Kovalenko <paul-kv@yandex.ru>
2014-09-15 20:15:52 +0400

    Use __intXX instead of raw types.

--- cs/engine/Common.hpp
+++ cs/engine/Common.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 using uint = unsigned int;
-using uint32 = unsigned int;
-using int32 = signed int;
-using ushort = unsigned short;
-using uint16 = unsigned short;
-using int16 = signed short;
-using uint64 = unsigned long long;
-using int64 = signed long long;
-using sbyte = signed char;
-using int8 = signed char;
+using uint32 = unsigned __int32;
+using int32 = __int32;
+using ushort = unsigned __int16;
+using uint16 = unsigned __int16;
+using int16 = __int16;
+using uint64 = unsigned __int64;
+using int64 = __int64;
+using sbyte = char;
+using int8 = char;
 using byte = unsigned char;
 using uint8 = unsigned char;
 
