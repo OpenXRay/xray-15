eece7597f3fb6cd0b25ec6626e05e7496dd04edf
Pavel Kovalenko <paul-kv@yandex.ru>
2014-09-16 19:14:58 +0400

    Remove DEFINE_STACK macro.

--- cs/engine/xrCore/_stl_extensions.h
+++ cs/engine/xrCore/_stl_extensions.h
@@ -228,9 +228,6 @@ struct pred_stri	: public std::binary_function<char*, char*, bool>	{
 	IC bool operator()(const char* x, const char* y) const				{	return stricmp(x,y)<0;	}
 };
 
-// STL extensions
-#define DEFINE_STACK(T,N)			typedef xr_stack< T > N;
-
 #include "FixedVector.h"
 #include "buffer_vector.h"
 
