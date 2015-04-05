f99768a51ead2ce11cd97c6d4a7a06eebd7d109c
Jules Blok <jules.blok@gmail.com>
2015-04-06 00:51:03 +0200

    xr_ioc_cmd: Don't set the R3 device flag when the GL renderer has been selected.

--- cs/engine/xrEngine/xr_ioc_cmd.cpp
+++ cs/engine/xrEngine/xr_ioc_cmd.cpp
@@ -474,7 +474,7 @@ public:
 		//	1..3 - r2
 		//	4 - r3
 		psDeviceFlags.set		(rsR2, ((renderer_value>0) && renderer_value<4) );
-		psDeviceFlags.set		(rsR3, (renderer_value>=4) );
+		psDeviceFlags.set		(rsR3, ((renderer_value>=4) && renderer_value<5) );
 		psDeviceFlags.set		(rsGL, (renderer_value>=5) );
 
 		r2_sun_static	= (renderer_value<2);
