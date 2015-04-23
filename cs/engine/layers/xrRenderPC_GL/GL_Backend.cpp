17833b3d0398111915a8064a3ee3ba7267a5f5f7
Jules Blok <jules.blok@gmail.com>
2015-04-24 00:41:56 +0200

    Implement GL_Backend.

--- cs/engine/layers/xrRenderPC_GL/GL_Backend.cpp
+++ cs/engine/layers/xrRenderPC_GL/GL_Backend.cpp
@@ -1,10 +1,79 @@
 #include "stdafx.h"
-#include "R_Backend.h"
+#include "../xrRender/R_Backend.h"
 
 CBackend			RCache;
-#include "stdafx.h"
-#pragma once
 
-#include "../xrRenderDX10/dx10BufferUtils.h"
+// Igor: is used to test bug with rain, particles corruption
+void CBackend::RestoreQuadIBData()
+{
+	const u32 dwTriCount = 4 * 1024;
+	u16		*Indices = 0;
 
-CBackend			RCache;
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, QuadIB);
+	Indices = (u16*)glMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY);
+	{
+		int		Cnt = 0;
+		int		ICnt = 0;
+		for (int i = 0; i<dwTriCount; i++)
+		{
+			Indices[ICnt++] = u16(Cnt + 0);
+			Indices[ICnt++] = u16(Cnt + 1);
+			Indices[ICnt++] = u16(Cnt + 2);
+
+			Indices[ICnt++] = u16(Cnt + 3);
+			Indices[ICnt++] = u16(Cnt + 2);
+			Indices[ICnt++] = u16(Cnt + 1);
+
+			Cnt += 4;
+		}
+	}
+	glUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+
+void CBackend::CreateQuadIB()
+{
+	const u32 dwTriCount = 4 * 1024;
+	const u32 dwIdxCount = dwTriCount * 2 * 3;
+	u16		*Indices = 0;
+	GLenum	dwUsage = GL_DYNAMIC_DRAW;
+
+	glGenBuffers(1, &QuadIB);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, QuadIB);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, dwIdxCount * 2, 0, dwUsage);
+	Indices = (u16*)glMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY);
+
+	int		Cnt = 0;
+	int		ICnt = 0;
+	for (int i = 0; i<dwTriCount; i++)
+	{
+		Indices[ICnt++] = u16(Cnt + 0);
+		Indices[ICnt++] = u16(Cnt + 1);
+		Indices[ICnt++] = u16(Cnt + 2);
+
+		Indices[ICnt++] = u16(Cnt + 3);
+		Indices[ICnt++] = u16(Cnt + 2);
+		Indices[ICnt++] = u16(Cnt + 1);
+
+		Cnt += 4;
+	}
+
+	glUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+// Device dependance
+void CBackend::OnDeviceCreate()
+{
+	CreateQuadIB();
+
+	// invalidate caching
+	Invalidate();
+}
+
+void CBackend::OnDeviceDestroy()
+{
+	// Quad
+	glDeleteBuffers(1, &QuadIB);
+}
