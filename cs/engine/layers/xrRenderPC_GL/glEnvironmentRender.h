52f54e45b6dd6e3c01f1d47252c6a0d17d442b19
Jules Blok <jules.blok@gmail.com>
2015-07-31 23:40:01 +0200

    xrRenderPC_GL: Implement glEnviromentRender.

--- cs/engine/layers/xrRenderPC_GL/glEnvironmentRender.h
+++ cs/engine/layers/xrRenderPC_GL/glEnvironmentRender.h
@@ -2,19 +2,76 @@
 
 #include "..\..\include\xrRender\EnvironmentRender.h"
 
+#include "..\xrRender\blenders\blender.h"
+class CBlender_skybox :
+	public IBlender
+{
+public:
+	virtual		LPCSTR		getComment()	{ return "INTERNAL: combiner"; }
+	virtual		BOOL		canBeDetailed()	{ return FALSE; }
+	virtual		BOOL		canBeLMAPped()	{ return FALSE; }
+
+	virtual		void		Compile(CBlender_Compile& C)
+	{
+		VERIFY(!"CBlender_skybox::Compile not implemented");
+	}
+};
+
+class glEnvDescriptorRender : public IEnvDescriptorRender
+{
+	friend class glEnvDescriptorMixerRender;
+public:
+	virtual void OnDeviceCreate(CEnvDescriptor &owner);
+	virtual void OnDeviceDestroy();
+
+	virtual void Copy(IEnvDescriptorRender &_in);
+private:
+	ref_texture			sky_texture;
+	ref_texture			sky_texture_env;
+	ref_texture			clouds_texture;
+};
+
+class glEnvDescriptorMixerRender : public IEnvDescriptorMixerRender
+{
+public:
+	virtual void Copy(IEnvDescriptorMixerRender &_in);
+
+	virtual void Destroy();
+	virtual void Clear();
+	virtual void lerp(IEnvDescriptorRender *inA, IEnvDescriptorRender *inB);
+	//private:
+public:
+	STextureList		sky_r_textures;
+	STextureList		sky_r_textures_env;
+	STextureList		clouds_r_textures;
+};
+
 class glEnvironmentRender :
 	public IEnvironmentRender
 {
 public:
 	glEnvironmentRender();
 
-	virtual void Copy(IEnvironmentRender &_in) { VERIFY(!"glEnvironmentRender::Copy not implemented."); };
-	virtual void OnFrame(CEnvironment &env) { VERIFY(!"glEnvironmentRender::OnFrame not implemented."); };
-	virtual void OnLoad() { VERIFY(!"glEnvironmentRender::OnLoad not implemented."); };
-	virtual void OnUnload() { VERIFY(!"glEnvironmentRender::OnUnload not implemented."); };
-	virtual void RenderSky(CEnvironment &env) { VERIFY(!"glEnvironmentRender::RenderSky not implemented."); };
-	virtual void RenderClouds(CEnvironment &env) { VERIFY(!"glEnvironmentRender::RenderClouds not implemented."); };
-	virtual void OnDeviceCreate() { VERIFY(!"glEnvironmentRender::OnDeviceCreate not implemented."); };
-	virtual void OnDeviceDestroy() { VERIFY(!"glEnvironmentRender::OnDeviceDestroy not implemented."); };
-	virtual particles_systems::library_interface const& particles_systems_library() { return (RImplementation.PSLibrary); };
+	virtual void Copy(IEnvironmentRender &_in);
+
+	virtual void OnFrame(CEnvironment &env);
+	virtual void OnLoad();
+	virtual void OnUnload();
+	virtual void RenderSky(CEnvironment &env);
+	virtual void RenderClouds(CEnvironment &env);
+	virtual void OnDeviceCreate();
+	virtual void OnDeviceDestroy();
+	virtual particles_systems::library_interface const& particles_systems_library();
+
+private:
+	CBlender_skybox			m_b_skybox;
+
+	ref_shader				sh_2sky;
+	ref_geom				sh_2geom;
+
+	ref_shader				clouds_sh;
+	ref_geom				clouds_geom;
+
+	ref_texture				tonemap;
+	ref_texture				tsky0, tsky1;
 };
