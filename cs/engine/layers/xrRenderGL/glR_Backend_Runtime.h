784a9e402b2bc4e94584e017abcfb3fc7b79583c
Jules Blok <jules.blok@gmail.com>
2015-09-21 22:05:09 +0200

    CBackend: Specify index offset as byte offset.

--- cs/engine/layers/xrRenderGL/glR_Backend_Runtime.h
+++ cs/engine/layers/xrRenderGL/glR_Backend_Runtime.h
@@ -175,7 +175,7 @@ ICF void CBackend::Render(D3DPRIMITIVETYPE T, u32 baseV, u32 startV, u32 countV,
 	stat.polys += PC;
 	constants.flush();
 	CHK_GL(glBindProgramPipeline(pp));
-	CHK_GL(glDrawElementsBaseVertex(Topology, iIndexCount, GL_UNSIGNED_SHORT, (void*)startI, baseV));
+	CHK_GL(glDrawElementsBaseVertex(Topology, iIndexCount, GL_UNSIGNED_SHORT, (void*)(startI * sizeof(GLushort)), baseV));
 	PGO(Msg("PGO:DIP:%dv/%df", countV, PC));
 }
 
