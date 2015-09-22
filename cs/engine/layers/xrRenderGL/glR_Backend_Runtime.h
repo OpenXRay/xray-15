96c39a6670cfc85ff37b375007d9e51ef920926a
Jules Blok <jules.blok@gmail.com>
2015-09-22 17:07:49 +0200

    CBackend: Reset depth mask after rendering.

--- cs/engine/layers/xrRenderGL/glR_Backend_Runtime.h
+++ cs/engine/layers/xrRenderGL/glR_Backend_Runtime.h
@@ -177,6 +177,9 @@ ICF void CBackend::Render(D3DPRIMITIVETYPE T, u32 baseV, u32 startV, u32 countV,
 	CHK_GL(glBindProgramPipeline(pp));
 	CHK_GL(glDrawElementsBaseVertex(Topology, iIndexCount, GL_UNSIGNED_SHORT, (void*)(startI * sizeof(GLushort)), baseV));
 	PGO(Msg("PGO:DIP:%dv/%df", countV, PC));
+
+	// Reset depth mask
+	CHK_GL(glDepthMask(GL_TRUE));
 }
 
 ICF void CBackend::Render(D3DPRIMITIVETYPE T, u32 startV, u32 PC)
@@ -191,6 +194,9 @@ ICF void CBackend::Render(D3DPRIMITIVETYPE T, u32 startV, u32 PC)
 	CHK_GL(glBindProgramPipeline(pp));
 	CHK_GL(glDrawArrays(Topology, startV, iIndexCount));
 	PGO(Msg("PGO:DIP:%dv/%df", iIndexCount, PC));
+
+	// Reset depth mask
+	CHK_GL(glDepthMask(GL_TRUE));
 }
 
 IC void CBackend::set_Geometry(SGeometry* _geom)
