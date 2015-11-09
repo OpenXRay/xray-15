394990f80acd0c6cf9d45bd93d1f157c042a4d9f
Jules Blok <jules.blok@gmail.com>
2015-11-09 09:48:34 +0100

    shaders: Fix vollight sampling in combine_volumetric.

--- cs/resources/shaders/gl/combine_volumetric.s
+++ cs/resources/shaders/gl/combine_volumetric.s
@@ -6,5 +6,5 @@ function normal		(shader, t_base, t_second, t_detail)
 --			: aref 		(true,0)	--	enable to save bandwith?
 			: sorting	(2, false)
 	shader:sampler	("s_vollight")  :texture	("$user$generic2")
-        shader:sampler 	("s_tonemap")   :texture        ("$user$tonemap")
+--    shader:sampler 	("s_tonemap")   :texture        ("$user$tonemap")
 end
