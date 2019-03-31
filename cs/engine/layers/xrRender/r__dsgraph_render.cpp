Almahmudrony/xray

Added actor shadow (by K.D.) which is enabled by default

cs/engine/layers/xrRender/r__dsgraph_render.cpp
@@ -675,6 +675,10 @@ void	R_dsgraph_structure::r_dsgraph_render_subspace	(IRender_Sector* _sector, CF
 				renderable->renderable_Render	();
 			}
 		}
+#if RENDER!=R_R1
+        if (g_pGameLevel && (phase == RImplementation.PHASE_SMAP) && ps_common_flags.test(RFLAG_ACTOR_SHADOW))
+            g_pGameLevel->pHUD->Render_Actor_Shadow();
+#endif
 	}
 
 	// Restore

OpenXRay/xray-15

cs/engine/layers/xrRender/r__dsgraph_render.cpp
@@ -675,6 +675,10 @@ void	R_dsgraph_structure::r_dsgraph_render_subspace	(IRender_Sector* _sector, CF
 				renderable->renderable_Render	();
 			}
 		}
+#if RENDER!=R_R1
+        if (g_pGameLevel && (phase == RImplementation.PHASE_SMAP) && ps_r2_ls_flags_ext.test(R2FLAGEXT_ACTOR_SHADOW))
+            g_pGameLevel->pHUD->Render_Actor_Shadow();
+#endif
 	}
 
 	// Restore