a8abc58b439b81828d42296bc7db6a5a8357fe1d
Xottab_DUTY <xottab-duty@yandex.ru>
2019-04-07 18:21:55 +0500

    Fix grass disappear in some frames

--- cs/engine/layers/xrRender/DetailManager.cpp
+++ cs/engine/layers/xrRender/DetailManager.cpp
@@ -256,7 +256,10 @@ void CDetailManager::UpdateVisibleM()
 	Fvector		EYE				= Device.vCameraPosition;
 	
 	CFrustum	View;
-	View.CreateFromMatrix		(Device.mFullTransform, FRUSTUM_P_LRTB + FRUSTUM_P_FAR);
+	//View.CreateFromMatrix		(Device.mFullTransform, FRUSTUM_P_LRTB + FRUSTUM_P_FAR);
+	/* KD: there is some bug: frustrum created from full transform matrix seems to be broken in some frames,
+	 * so we should use saved frustrum from render interface */
+	View = RImplementation.ViewBase;
 
 	float fade_limit			= dm_fade;	fade_limit=fade_limit*fade_limit;
 	float fade_start			= 1.f;		fade_start=fade_start*fade_start;
