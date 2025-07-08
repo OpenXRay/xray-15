0c7cd53c80e8e5294c83c0f297e7a5c0bbd6f6e5
tyabus <tyabustest@gmail.com>
2025-07-08 17:35:04 +0500

    xrRender: Abort occlusion call when the ID is invalid not only on release builds

--- cs/engine/layers/xrRender/r__occlusion.cpp
+++ cs/engine/layers/xrRender/r__occlusion.cpp
@@ -42,13 +42,11 @@ u32		R_occlusion::occq_begin		(u32&	ID		)
 	if (!enabled)		return 0;
 
 	//	Igor: prevent release crash if we issue too many queries
-//#ifndef	DEBUG
 	if (pool.empty())
 	{
 		ID = iInvalidHandle;
 		return 0;
 	}
-//#endif	//	DEBUG
 
 	RImplementation.stats.o_queries	++;
 	if (!fids.empty())	{
@@ -71,12 +69,7 @@ u32		R_occlusion::occq_begin		(u32&	ID		)
 }
 void	R_occlusion::occq_end		(u32&	ID		)
 {
-	if (!enabled)		return;
-
-	//	Igor: prevent release crash if we issue too many queries
-#ifndef	DEBUG
-	if (ID == iInvalidHandle) return;
-#endif	//	DEBUG
+	if (!enabled || ID == iInvalidHandle)		return;
 
 	// Msg				("end  : [%2d] - %d", used[ID].order, ID);
 	//CHK_DX			(used[ID].Q->Issue	(D3DISSUE_END));
@@ -84,12 +77,7 @@ void	R_occlusion::occq_end		(u32&	ID		)
 }
 R_occlusion::occq_result R_occlusion::occq_get		(u32&	ID		)
 {
-	if (!enabled)		return 0xffffffff;
-
-	//	Igor: prevent release crash if we issue too many queries
-#ifndef	DEBUG
-	if (ID == iInvalidHandle) return 0xFFFFFFFF;
-#endif	//	DEBUG
+	if (!enabled || ID == iInvalidHandle)		return 0xffffffff;
 
 	occq_result	fragments	= 0;
 	HRESULT hr;
